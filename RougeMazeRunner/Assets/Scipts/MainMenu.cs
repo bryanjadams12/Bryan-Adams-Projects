29d2d54f3455f6cff544b9bf01041edb9c23457a
bryanjadams12 <bryanjadams77@gmail.com>
2025-05-11 14:38:14 -0700

    UI start
    this is just the beginning i will work to add sound effects and more functionality later

---
+++ RougeMazeRunner/Assets/Scipts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Load the game scene
+        SceneManager.LoadSceneAsync(1);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
